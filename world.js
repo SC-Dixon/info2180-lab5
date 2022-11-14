7b9c7225169adf620ef307bfce9a259683ca4d07
SC-Dixon <shantay1234.sd@gmail.com>
2022-11-14 16:55:04 -0500

    Updated world.css, index.html, world.php, world.js files to show cities table. Excercise 5 completed.

--- world.js
+++ world.js
@@ -1,13 +1,39 @@
 $(document).ready(function() {
-    var lookupBtn = $('#lookup');
+    var lookupCtryBtn = $('#lookupCtry');
+    var lookupCitBtn = $('#lookupCit');
 
-    lookupBtn.on('click', function() {
+    lookupCtryBtn.on('click', function() {
         var cQuery = $('#country').val();
+        var citQuery = 'NA';
 
         $.ajax({
             url:'world.php?country='+cQuery,
             method: "GET",
-            data:{country:cQuery}
+            data:{country:cQuery,lookup:citQuery}
+        }).done(function(response) {
+            
+            var resultDiv = document.querySelector('#result');
+            resultDiv.innerHTML = response;
+ 
+            
+
+        }).fail(function() {
+            alert('There was a problem with the request.');
+        });
+
+
+
+    
+    });
+
+    lookupCitBtn.on('click', function() {
+        var cQuery = $('#country').val();
+        var citQuery = 'cities';
+
+        $.ajax({
+            url:'world.php?country='+cQuery+'&lookup='+citQuery,
+            method: "GET",
+            data:{country:cQuery,lookup:citQuery}
         }).done(function(response) {
             
             var resultDiv = document.querySelector('#result');
--- world.php
+++ world.php
@@ -5,29 +5,60 @@ $password = 'password123';
 $dbname = 'world';
 
 $country = $_GET["country"];
+$cities = $_GET["lookup"];
 $country = filter_var($country, FILTER_SANITIZE_STRING);
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if($cities === 'NA'){
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  countryCall($results);
+
+}elseif($cities === 'cities'){
+  $stmt = $conn->query("SELECT c.name, c.district, c.population  FROM cities c join countries cs on c.country_code = cs.code  WHERE cs.name LIKE '%$country%'");
+
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  cityCall($results);
+}
+
+
+
 
 ?>
-<table id="cTable">
-  <tr>
-    <th scope="col">Name</th>
-    <th scope="col">Continent</th>
-    <th scope="col">Independence</th>
-    <th scope="col">Head of State</th>
-  </tr>
-<?php foreach ($results as $row): ?>
-  <tr>
-    <td><?= $row['name'] ?></td>
-    <td><?= $row['continent'] ?></td>
-    <td><?= $row['independence_year'] ?></td>
-    <td><?= $row['head_of_state'] ?></td>
-  </tr>
-<?php endforeach; ?>
-</table>
 
+<?php function countryCall($results){ ?>
+  <table id="cTable">
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Continent</th>
+      <th scope="col">Independence</th>
+      <th scope="col">Head of State</th>
+    </tr>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['continent'] ?></td>
+      <td><?= $row['independence_year'] ?></td>
+      <td><?= $row['head_of_state'] ?></td>
+    </tr>
+  <?php endforeach; ?>
+  </table>
+<?php } ?>
 
+<?php function cityCall($results){ ?>
+  <table id="citTable">
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">District</th>
+      <th scope="col">Population</th>
+    </tr>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['district'] ?></td>
+      <td><?= $row['population'] ?></td>
+    </tr>
+  <?php endforeach; ?>
+  </table>
+<?php } ?>
